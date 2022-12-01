c88aa5cac4ad6bb318c84db5926e1b5e02c51350
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-01 23:31:19 +0900

    wall, door render

--- quest.py
+++ quest.py
@@ -6,6 +6,18 @@ GRID_SIZE=50
 
 WIDTH = GRID_WIDTH * GRID_SIZE
 HEIGHT = GRID_HEIGHT * GRID_SIZE
+MAP=["WWWWWWWWWWWWWWWW",
+     "W           K  W",
+     "W  K           W",
+     "W  W  KG       W",
+     "W  WWWWWWWWWW  W",
+     "W         K    W",
+     "WK     P       W",
+     "W  WWWWWWWWWW  W",
+     "W      GK   W  W",
+     "W              W",
+     "W  K       K   D",
+     "WWWWWWWWWWWWWWWW"]
 
 def screen_coords(x, y):
   return(x * GRID_SIZE, y * GRID_SIZE)
@@ -15,7 +27,16 @@ def draw_background():
     for x in range(GRID_WIDTH):
       screen.blit("floor1", screen_coords(x, y))
 
+def draw_scenery():
+  for y in range(GRID_HEIGHT):
+    for x in range(GRID_WIDTH):
+      square = MAP[y][x]
+      if square == "W":
+        screen.blit("wall", screen_coords(x, y))
+      elif square == "D":
+        screen.blit("door", screen_coords(x, y))
+
 def draw():
   draw_background()
-
+  draw_scenery()
 pgzrun.go()
