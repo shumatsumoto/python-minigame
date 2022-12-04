041dba26f30321530122126408d236d365b75a32
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-05 00:16:33 +0900

    ball表示

--- quest.py
+++ quest.py
@@ -22,14 +22,21 @@ MAP=["WWWWWWWWWWWWWWWW",
 def screen_coords(x, y):
   return(x * GRID_SIZE, y * GRID_SIZE)
 
+def grid_coords(actor):
+  return(round(actor.x / GRID_SIZE), round(actor.y / GRID_SIZE))
+
 def setup_game():
-  global player
+  global player, keys_to_collect
   player = Actor("player", anchor = ("left", "top"))
+  keys_to_collect = []
   for y in range(GRID_HEIGHT):
     for x in range(GRID_WIDTH):
       square = MAP[y][x]
       if square == "P":
         player.pos = screen_coords(x, y)
+      elif square == "K":
+        key = Actor("key", anchor = ("left", "top"), pos = screen_coords(x, y))
+        keys_to_collect.append(key)
 
 def draw_background():
   for y in range(GRID_HEIGHT):
@@ -47,6 +54,29 @@ def draw_scenery():
 
 def draw_actors():
   player.draw()
+  for key in keys_to_collect:
+    key.draw()
+
+def on_key_down(key):
+  if key==keys.LEFT:
+    move_player(-1,0)
+  elif key==keys.UP:
+    move_player(0,-1)
+  elif key==keys.RIGHT:
+    move_player(1,0)
+  elif key==keys.DOWN:
+    move_player(0,1)
+
+def move_player(dx, dy):
+  (x, y) = grid_coords(player)
+  x += dx
+  y += dy
+  square = MAP[x][y]
+  if square == "W":
+    return
+  elif square == "D":
+    return
+
 
 def draw():
   draw_background()
